fc01d2e0993c9af932088c44b2e5fae5d697ce84
jjruanlili <525162140@qq.com>
2019-11-08 15:53:54 +0800

    修正i的值
    i应该小于length-1

--- SelectionSort.java
+++ SelectionSort.java
@@ -8,7 +8,7 @@ package sort.Sorts;
 public class SelectionSort {
     public static void select(int[] arr){
         int tmp = 0;
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0; i < arr.length-1; i++){
             for(int j = i+1; j < arr.length; j++){
                 if(arr[j] < arr[i]){
                     tmp = arr[i];
